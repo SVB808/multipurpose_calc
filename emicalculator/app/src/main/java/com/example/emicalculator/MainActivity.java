17ca0e171af4a8a499de32c7377a9b80b6456ca3
SVB808 <shruthavbhat@gmail.com>
2022-07-08 21:57:18 +0530

    fifth

--- emicalculator/app/src/main/java/com/example/emicalculator/MainActivity.java
+++ emicalculator/app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.emicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        hlth = (ImageButton) findViewById(R.id.health);
+        fina = (ImageButton) findViewById(R.id.finance);
+        mths = (ImageButton) findViewById(R.id.maths);
+    }
+    public void movepageh(View v)
+    {
+        Intent jumph = new Intent(MainActivity.this,select_use_health.class);
+        startActivity(jumph);
+    }
+    public void movepagef(View v)
+    {
+        Intent jumpf = new Intent(MainActivity.this,select_use_finance.class);
+        startActivity(jumpf);
+    }
+    public void movepagem(View v)
+    {
+        Intent jumpm = new Intent(MainActivity.this,select_use_math.class);
+        startActivity(jumpm);
     }
 }
--- emicalculator/app/src/main/java/com/example/emicalculator/select_use_health.java
+++ emicalculator/app/src/main/java/com/example/emicalculator/select_use_health.java
@@ -2,13 +2,28 @@ package com.example.emicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class select_use_health extends AppCompatActivity {
-
+    Button bmib,bfatb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_use_health);
+        bmib = (Button) findViewById(R.id.bmi);
+        bfatb = (Button) findViewById(R.id.bfat);
+    }
+    public void bmicalc (View v)
+    {
+        Intent jbmi = new Intent(select_use_health.this,bmicalc.class);
+        startActivity(jbmi);
+    }
+    public void bfatcalc (View v)
+    {
+        Intent jbfat = new Intent(select_use_health.this,bfatcalc.class);
+        startActivity(jbfat);
     }
 }
