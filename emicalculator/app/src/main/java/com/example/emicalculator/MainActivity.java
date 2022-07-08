e7b2ab2caa023e62769fb57d775a1acffcc33733
SVB808 <shruthavbhat@gmail.com>
2022-07-08 21:03:04 +0530

    fourth

--- emicalculator/app/src/main/java/com/example/emicalculator/MainActivity.java
+++ emicalculator/app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+    ImageButton hlth,fina,mths;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
