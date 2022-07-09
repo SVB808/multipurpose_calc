e500a044093a55e257b35ee5109deaa72f3c4d4f
SVB808 <shruthavbhat@gmail.com>
2022-07-09 21:13:31 +0530

    eleventh

--- emicalculator/app/src/main/java/com/example/emicalculator/cicalc.java
+++ emicalculator/app/src/main/java/com/example/emicalculator/cicalc.java
@@ -1,13 +1,12 @@
-package com.example.cinterest;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.emicalculator;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class cicalc extends AppCompatActivity {
     EditText t,roi,p;
     TextView res;
     @Override
@@ -16,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cicalc);
         p=findViewById(R.id.princ);
         roi=findViewById(R.id.roi);
-        t=findViewById(R.id.time);
-        res=findViewById(R.id.res);
+        t=findViewById(R.id.ctime);
+        res=findViewById(R.id.ciresult);
     }
-    public void calc(View v){
+    public void valci(View v){
         float a,b,c;
         a = Float.parseFloat(p.getText().toString());
         b = Float.parseFloat(roi.getText().toString());
@@ -30,7 +29,5 @@ public class MainActivity extends AppCompatActivity {
         String rr;
         rr = String.format("%.2f",ra);
         res.setText(rr);
-
-
     }
 }
