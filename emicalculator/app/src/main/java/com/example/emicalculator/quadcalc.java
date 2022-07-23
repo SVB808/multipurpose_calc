d1b67bcd8bd5740cc85eb8b574a97294df86975c
SVB808 <shruthavbhat@gmail.com>
2022-07-23 22:23:52 +0530

    sixteenth

--- emicalculator/app/src/main/java/com/example/emicalculator/quadcalc.java
+++ emicalculator/app/src/main/java/com/example/emicalculator/quadcalc.java
@@ -6,28 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 public class quadcalc extends AppCompatActivity {
-    EditText a,b,c;
+    EditText a, b, c;
     TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quadcalc);
-        a=findViewById(R.id.aq);
-        b=findViewById(R.id.bq);
-        c=findViewById(R.id.cq);
-        t=findViewById(R.id.qr);
+        a = findViewById(R.id.aq);
+        b = findViewById(R.id.bq);
+        c = findViewById(R.id.cq);
+        t = findViewById(R.id.qr);
 
     }
-    public void  calq(View v){
-        float a1,b1,c1,r1,r2;
-        a1=Float.parseFloat(a.getText().toString());
-        b1=Float.parseFloat(b.getText().toString());
-        c1=Float.parseFloat(c.getText().toString());
-        r1= (float) (-b1+(Math.sqrt((b1*b1)-(4*a1*c1))))/(2*a1);
-        r2= (float) (-b1-(Math.sqrt((b1*b1)-(4*a1*c1))))/(2*a1);
-        t.setText("" + String.format("%.2f", r1)+", "+String.format("%.2f",r2));
+
+    public void calq(View v) {
+        float a1, b1, c1, r1, r2;
+        a1 = Float.parseFloat(a.getText().toString());
+        b1 = Float.parseFloat(b.getText().toString());
+        c1 = Float.parseFloat(c.getText().toString());
+        r1 = (float) (-b1 + (Math.sqrt((b1 * b1) - (4 * a1 * c1)))) / (2 * a1);
+        r2 = (float) (-b1 - (Math.sqrt((b1 * b1) - (4 * a1 * c1)))) / (2 * a1);
+        String re1, re2;
+        re1 = String.format("%.2f", r1);
+        re2 = String.format("%.2f", r2);
+        t.setText(re1 + ", " + re2);
 
     }
 }
